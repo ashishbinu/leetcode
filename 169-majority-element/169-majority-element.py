75a89d82b295e8eb82e93d5215793cc00277b525
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-22 15:56:16 +0530

    Time: 399 ms (5.02%), Space: 15.6 MB (34.89%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,17 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Boyer Moore algorithm - https://www.youtube.com/watch?v=7pnhv842keE
-        res = count = 0
-        for i in nums:
-            if not count:
-                res,count = i,1
-                continue
-            if i != res:
-                count -= 1
-            else:
-                count += 1
-                
-        return res
+        counter = {}
+        res,count = 0,0
+        for v in nums:
+            if v not in counter: counter[v] = 0
+            counter[v] += 1
+            if counter[v] > count:
+                count = counter[v]
+                res = v
+        return res    
+    
+        # # Boyer Moore algorithm - https://www.youtube.com/watch?v=7pnhv842keE
+        # # time - O(n), space - O(1)
+        # res = count = 0
+        # for i in nums:
+        #     if not count:
+        #         res,count = i,1
+        #         continue
+        #     if i != res:
+        #         count -= 1
+        #     else:
+        #         count += 1
+        #         
+        # return res
         
         # counter = Counter(nums)
         # return counter.most_common(1)[0][0]
