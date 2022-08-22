bb5c6270977f1fb10b86880fca72e8ae7f014a18
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-23 01:24:13 +0530

    Time: 90 ms (72.15%), Space: 15.1 MB (50.22%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,7 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         numdict = {}
-        
         for i,v in enumerate(nums):
             sval = target - v 
             if sval in numdict: return [i,numdict[sval]]
