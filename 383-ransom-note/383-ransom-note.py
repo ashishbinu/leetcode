1ce8170d9089eeb3b452edf8f81cd831f2b66150
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-22 13:47:49 +0530

    Time: 83 ms (60.75%), Space: 14.2 MB (53.79%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine): return False
+        counter = {}
+        
+        for a in magazine:
+            if a not in counter: counter[a] = 0
+            counter[a] += 1
+            
+        for b in ransomNote:
+            if b not in counter: counter[b] = 0
+            counter[b] -= 1
+                
+        for c in ransomNote:
+            if counter[c] < 0: return False
+        
+        return True
+        
+        
+        
