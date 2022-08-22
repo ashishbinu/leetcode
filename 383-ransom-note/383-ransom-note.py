b1246691ae51250fc1c31e2b975b575397861c1e
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-22 14:26:09 +0530

    Time: 46 ms (96.75%), Space: 14.1 MB (94.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,18 +1,25 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        if len(ransomNote) > len(magazine): return False
-        counter = {}
-        
-        for a in magazine:
-            if a not in counter: counter[a] = 0
-            counter[a] += 1
-            
-        for b in ransomNote:
-            if b not in counter: counter[b] = 0
-            counter[b] -= 1
-            if counter[b] < 0: return False
-        
+        # using builtin Counter object to speed up the algorithm
+        counter = Counter(magazine)
+        counter.subtract(ransomNote)
+        if counter.most_common()[-1][1] < 0: return False
         return True
+        
+        # time - O(n) , space - O(n)
+        # if len(ransomNote) > len(magazine): return False
+        # counter = {}
+        # 
+        # for a in magazine:
+        #     if a not in counter: counter[a] = 0
+        #     counter[a] += 1
+        #     
+        # for b in ransomNote:
+        #     if b not in counter: counter[b] = 0
+        #     counter[b] -= 1
+        #     if counter[b] < 0: return False
+        # 
+        # return True
         
         
         
