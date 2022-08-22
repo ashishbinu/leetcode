3f4c13c609ff6805865165881ba3b5abcd1ced89
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-22 14:45:09 +0530

    Time: 133 ms (20.84%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,30 +1,31 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
-        # using builtin Counter object to speed up the algorithm
-        counter = Counter(magazine)
-        counter.subtract(ransomNote)
-        # Finds the least common element that is negative
-        if counter.most_common()[-1][1] < 0: return False
+        # time - O(n) , space - O(n), can use array of alphabets instead of hashmap
+        if len(ransomNote) > len(magazine): return False
+        counter = {}
+        
+        for a in magazine:
+            if a not in counter: counter[a] = 0
+            counter[a] += 1
+            
+        for b in ransomNote:
+            if b not in counter: counter[b] = 0
+            counter[b] -= 1
+            if counter[b] < 0: return False
+        
         return True
+        
+        # # using builtin Counter object to speed up the algorithm
+        # counter = Counter(magazine)
+        # counter.subtract(ransomNote)
+        # # Finds the least common element that is negative
+        # if counter.most_common()[-1][1] < 0: return False
+        # return True
         
         # counter subtraction removes negative count
         # return not Counter(ransomNote) - Counter(magazine)
         
-        # # time - O(n) , space - O(n)
-        # if len(ransomNote) > len(magazine): return False
-        # counter = {}
-        # 
-        # for a in magazine:
-        #     if a not in counter: counter[a] = 0
-        #     counter[a] += 1
-        #     
-        # for b in ransomNote:
-        #     if b not in counter: counter[b] = 0
-        #     counter[b] -= 1
-        #     if counter[b] < 0: return False
-        # 
-        # return True
         
         
         
