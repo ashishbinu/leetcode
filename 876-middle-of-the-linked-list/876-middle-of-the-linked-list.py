395ae104719155d248e1f317a94930cd4f54f78c
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-25 01:38:05 +0530

    Time: 43 ms (65.18%), Space: 13.7 MB (95.85%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -7,7 +7,6 @@ class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         i = j = head
         while j and j.next:
-            # if j.next is None: break
             i = i.next
             j = j.next.next
             
