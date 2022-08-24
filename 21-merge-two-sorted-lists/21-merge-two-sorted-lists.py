a8c9a8deacb2767d92d04d246ffcb51f4e64d5e9
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-25 00:47:26 +0530

    Time: 44 ms (83.17%), Space: 13.9 MB (32.84%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -6,20 +6,42 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        list3 = ptr = ListNode()
+        # # Creating new list space - O(n)
+        # list3 = ptr = ListNode()
+        # 
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         ptr.next = ListNode(list1.val)
+        #         list1 = list1.next
+        #     else:
+        #         ptr.next = ListNode(list2.val)
+        #         list2 = list2.next
+        #     
+        #     ptr = ptr.next
+        # 
+        # # instead of creating new list for rest of the values just point the remaining list onto the merged list
+        # if list1: ptr.next = list1
+        # if list2: ptr.next = list2
+        #     
+        # return list3.next
+        
+        res = ListNode()
+        back = res
+        back.next = list1
+        
         
         while list1 and list2:
-            if list1.val < list2.val:
-                ptr.next = ListNode(list1.val)
-                list1 = list1.next
+            
+            if list2.val < list1.val:
+                back.next = list2
+                tmp = list2.next
+                list2.next = list1
+                back = list2
+                list2 = tmp
             else:
-                ptr.next = ListNode(list2.val)
-                list2 = list2.next
+                back = list1
+                list1 = list1.next
+                
+        if list2: back.next = list2        
             
-            ptr = ptr.next
-        
-        # instead of creating new list for rest of the values just point the remaining list onto the merged list
-        if list1: ptr.next = list1
-        if list2: ptr.next = list2
-            
-        return list3.next
+        return res.next
