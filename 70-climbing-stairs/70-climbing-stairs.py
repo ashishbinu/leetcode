c821aa563cce29dce69d8e2b107859ead140bbd4
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-25 20:14:47 +0530

    Time: 59 ms (16.79%), Space: 13.8 MB (96.38%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,13 +1,17 @@
 class Solution:
     def climbStairs(self, n: int,memo={}) -> int:
+        # It's basically fibonacci with fib(0) = 1 and fib(1) = 1
         
-        # It's basically fibonacci
-        
-        def cs(n,a,b):
-            if n == 0: return a
-            if n == 1: return b
-            
-            return cs(n-1,b,a+b)
-        
-        return cs(n,1,1)
+        a,b = 1,1
+        for i in range(1,n):
+            a,b = b,a+b
+        return b
+    
+        # def cs(n,a,b):
+        #     if n == 0: return a
+        #     if n == 1: return b
+        #     
+        #     return cs(n-1,b,a+b)
+        # 
+        # return cs(n,1,1)
         
