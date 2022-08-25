8126802c4b6a8a43692d6b4d56e7092c8a2ef79a
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-25 21:46:23 +0530

    Time: 1152 ms (87.10%), Space: 25 MB (38.55%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        # Works on kadane's algorithm
-        
+        # Two pointer approach
+        l,r = 0,1
         max_profit = 0
-        s = 0
-        for k,v in enumerate(prices):
-            if k == 0: continue # skip for first index
-            pd = prices[k] - prices[k-1]
-            s += pd
-            if s < 0: s = 0
-            max_profit = max(s,max_profit)
-            
-        return max_profit    
+        while r < len(prices):
+            profit = prices[r] - prices[l]
+            if profit < 0:
+                l = r
+            max_profit = max(profit,max_profit)
+            r += 1
+        return max_profit
+    
+        # # Works on kadane's algorithm
+        # max_profit = 0
+        # s = 0
+        # for k,v in enumerate(prices):
+        #     if k == 0: continue # skip for first index
+        #     pd = prices[k] - prices[k-1]
+        #     s += pd
+        #     if s < 0: s = 0
+        #     max_profit = max(s,max_profit)
+        #     
+        # return max_profit    
             
             
             
