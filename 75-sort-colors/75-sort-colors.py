7ae052d6420a028dfae23b7ef98bb8b2b5fee263
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-29 00:17:56 +0530

    Time: 44 ms (73.40%), Space: 14 MB (15.87%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -8,14 +8,16 @@ class Solution:
         r = n - 1
         
         while i < r:
-            while nums[r] == 2:
-                r-=1 
-                if r < 0 and l > r: return
             while nums[l] == 0:
                 l+=1 
                 if l >= n or l > r: return 
-            while i < r and nums[i] == 1 or i < l: i+=1 
-            if nums[i] == 2:
-                nums[i],nums[r] = nums[r],nums[i]
-            elif nums[i] == 0:
-                nums[i],nums[l] = nums[l],nums[i]
+            while nums[r] == 2:
+                r-=1 
+                if r < 0 or l > r: return
+            if i < l: i = l    
+            while nums[i] == 1:
+                i+=1 
+                if i > r: return
+                
+            if nums[i] == 2: nums[i],nums[r] = nums[r],nums[i]
+            elif nums[i] == 0: nums[i],nums[l] = nums[l],nums[i]
