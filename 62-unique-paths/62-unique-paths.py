c6c3c7550963be3246bcbafeb77b892b6696e694
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-08-30 23:47:00 +0530

    Time: 70 ms (7.02%), Space: 13.8 MB (74.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,11 +1,22 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0 for _ in range(n)] for _ in range(m)]
-        dp[0][0] = 1
+        # iterative dp with space optimisation
+        prev = [0] * n
+        curr = [0] * n
+        curr[0] = 1
         for i in range(m):
             for j in range(n):
-                if i - 1 >= 0: dp[i][j] += dp[i-1][j] 
-                if j - 1 >= 0: dp[i][j] += dp[i][j-1]
+                if i-1 >= 0: curr[j] = prev[j]
+                if j-1 >= 0: curr[j] += curr[j-1]
+            prev = curr
+        return curr[n-1]
         
-        return dp[m-1][n-1]
-        
+        # dp = [[0 for _ in range(n)] for _ in range(m)]
+        # dp = [[0 for _ in range(n)] for _ in range(m)]
+        # dp[0][0] = 1
+        # for i in range(m):
+        #     for j in range(n):
+        #         if i - 1 >= 0: dp[i][j] += dp[i-1][j] 
+        #         if j - 1 >= 0: dp[i][j] += dp[i][j-1]
+        # 
+        # return dp[m-1][n-1]
