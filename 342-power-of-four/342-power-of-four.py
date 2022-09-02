8919ce500b0a4d1c8764a2a0467e55852ae467f4
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-02 18:02:59 +0530

    Time: 47 ms (60.90%), Space: 13.8 MB (95.04%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,5 +1,12 @@
 class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
+    def isPowerOfThree(self, n: int) -> bool:
+        # iterative
         if n < 1: return False
-        while n%4==0 and n>1: n//=4
+        while n%3==0 and n>1: n//=3
         return n == 1
+            
+        # # recursive
+        # if n == 1: return True
+        # if n%3 != 0 or n < 1: return False
+        # return self.isPowerOfThree(n//3)
+        
