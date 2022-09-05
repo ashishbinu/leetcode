4f0860ad41a579efa8ffec71ec33788c5df5372a
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-06 00:33:18 +0530

    Time: 4403 ms (46.88%), Space: 14.2 MB (83.16%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -5,10 +5,6 @@ class Solution:
         dp = [0] * n
         dp[0] = 1
         for i in range(1,n):
-            dp[i] = 1 
-            m = 0
-            for k in range(1,i+1):
-                if nums[i] > nums[i-k] and m < dp[i-k]:
-                    m = dp[i-k]
-            dp[i] += m
+            dp[i] = 1 + max([dp[j] for j in range(i) if nums[i] > nums[j]],default=0) 
+            
         return max(dp)
