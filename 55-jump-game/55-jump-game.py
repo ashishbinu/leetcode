8a3986bddbac0b9fffe42e5ecb214194d25e306f
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-12 17:28:36 +0400

    Time: 3109 ms (10.31%), Space: 15.3 MB (18.17%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,19 +1,20 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # greedy approach
+        # dp approach
         n = len(nums)
-        res = n - 1
-        for i in range(n-1,-1,-1):
-            if nums[i] - (res - i) >= 0:
-                res = i
-        return not res
+        dp = [False] * n
+        dp[0] = True
+        for i in range(1,n):
+            for j in reversed(range(i)):
+                dp[i] = dp[j] and j + nums[j] >= i
+                if dp[i]: break
+                    
+        return dp[-1]
     
-        # # dp apprach
+        # # greedy approach
         # n = len(nums)
-        # dp = [False] * n
-        # dp[0] = True
-        # for i in range(1,n):
-        #     for j in reversed(range(i)):
-        #         dp[i] = dp[j] and nums[j] >= i - j
-        #         if dp[i]: break
-        # return dp[n-1]
+        # res = n - 1
+        # for i in range(n-1,-1,-1):
+        #     if nums[i] - (res - i) >= 0:
+        #         res = i
+        # return not res
