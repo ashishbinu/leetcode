ba54c7408d5591e95f15e5f26ab5d44e093bf206
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-12 21:20:29 +0400

    Time: 96 ms (85.68%), Space: 14.5 MB (37.99%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,11 +1,26 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [1]*n
-        min_dp = [1]*n
-        dp[0] = nums[0]
-        min_dp[0] = nums[0]
-        for i in range(1,n):
-            dp[i] = max(nums[i],min_dp[i-1]*nums[i],dp[i-1]*nums[i])
-            min_dp[i] = min(nums[i],min_dp[i-1]*nums[i],dp[i-1]*nums[i])
-        return max(dp)
+        a,b,c = 1,1,1 # a = dp[i-1], b = min_dp[i-1], c = dp[i]
+        res = -math.inf
+        for i,v in enumerate(nums):
+            c = max(a*v,b*v,v)
+            b = min(a*v,b*v,v)
+            a = c
+            res = max(res,c)
+        return res
+    
+        # # dp approach
+        # # dp[i] stores the max product subarray which includes i in the array till i 
+        # # min_dp[i] stores the min product subarray which includes i in the array till i 
+        # n = len(nums)
+        # dp = [1]*n
+        # min_dp = [1]*n
+        # dp[0] = nums[0]
+        # min_dp[0] = nums[0]
+        # for i in range(1,n):
+        #     dp[i] = max(nums[i],min_dp[i-1]*nums[i],dp[i-1]*nums[i])
+        #     # min_dp is there to store the min product in the case to find max in [-2,3,-4] 
+        #     min_dp[i] = min(nums[i],min_dp[i-1]*nums[i],dp[i-1]*nums[i])
+        #     
+        # return max(dp)
