899beacf1c8c94ecf625c6e8443d4d0a554f02af
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-17 22:00:39 +0400

    Time: 103 ms (19.60%), Space: 14.8 MB (6.63%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -4,10 +4,9 @@ class Solution:
         l,r = -1,n
         while l + 1 < r:
             m = l+ (r-l)//2
-            if target < nums[m]:
+            if target <= nums[m]:
                 r = m
             else:
                 l = m
-        if nums[l] != target: l+=1
-        return l
+        return r
                 
