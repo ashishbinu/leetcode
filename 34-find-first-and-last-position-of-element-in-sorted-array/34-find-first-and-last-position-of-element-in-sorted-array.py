ad84c2f4b545ef9cbcce64068dfc647c0bc5d3d9
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-18 16:31:09 +0400

    Time: 187 ms (11.96%), Space: 15.4 MB (92.14%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,22 +1,30 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        l,r = -1,n
-        while l + 1 < r:
-            m = l + (r - l)//2
-            if target <= nums[m]:
-                r = m
-            else:
-                l = m
-        a = r if -1 < r < n and nums[r] == target else -1
-        
-        l,r = a,n
-        while l + 1 < r:
-            m = l + (r - l)//2
-            if nums[m] <= target:
-                l = m
-            else:
-                r = m
-        b = l if -1 < l < n and nums[l] == target else -1
-        
+        # using bisect
+        a = bisect_left(nums,target)
+        b = bisect_right(nums,target) - 1
+        a = a if -1 < a < len(nums) and nums[a] == target else -1
+        b = b if -1 < b < len(nums) and nums[b] == target else -1
         return [a,b]
+    
+        # # binary search
+        # n = len(nums)
+        # l,r = -1,n
+        # while l + 1 < r:
+        #     m = l + (r - l)//2
+        #     if target <= nums[m]:
+        #         r = m
+        #     else:
+        #         l = m
+        # a = r if -1 < r < n and nums[r] == target else -1
+        # 
+        # l,r = a,n # l is changed to a instead of -1 as it reduced the search space
+        # while l + 1 < r:
+        #     m = l + (r - l)//2
+        #     if nums[m] <= target:
+        #         l = m
+        #     else:
+        #         r = m
+        # b = l if -1 < l < n and nums[l] == target else -1
+        # 
+        # return [a,b]
