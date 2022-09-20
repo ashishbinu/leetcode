fedd8fb07d635a4cd85acbdf8a2041b4723be1a0
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-20 20:18:21 +0400

    Time: 384 ms (32.05%), Space: 15.2 MB (34.92%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,16 +1,20 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        # using arraylist
+        def idx(key): return ord(key) - ord('a')
+        
         n,N = len(p),len(s)
         res = []
         if n > N: return res
         
+        
         # we can replace these dict with array of 26 length
-        freq_p = defaultdict(int)
-        window = defaultdict(int) # n length string(window) in s
+        freq_p = [0] * 26
+        window = [0] * 26 # n length string(window) in s
         
         for i in range(n):
-            freq_p[p[i]] += 1
-            window[s[i]] += 1
+            freq_p[idx(p[i])] += 1
+            window[idx(s[i])] += 1
         
         is_anagram = window == freq_p
         if is_anagram: res.append(0)
@@ -19,17 +23,48 @@ class Solution:
             # This is_anagram here represents if the previous window value was anagram or not
             
             i = j - n # i represents previous windows starting position
-            window[s[i]] -= 1
-            window[s[j]] += 1
+            window[idx(s[i])] -= 1
+            window[idx(s[j])] += 1
             
-            if window[s[i]]==0: del window[s[i]] # remove character from dict if value is zero
-            
-            
-            # hashtable comparison will take O(1)
-            # as max number of comparison is 26 (only lower case alphabets)
+            # freqp==window - O(1) - max number of comparison is 26 (only lower case alphabets)
             is_anagram = (is_anagram and s[i]==s[j]) or freq_p==window
             
             if is_anagram: res.append(i+1)
                 
         return res
+        
+        # # using hashmaps
+        # n,N = len(p),len(s)
+        # res = []
+        # if n > N: return res
+        # 
+        # 
+        # # we can replace these dict with array of 26 length
+        # freq_p = defaultdict(int)
+        # window = defaultdict(int) # n length string(window) in s
+        # 
+        # for i in range(n):
+        #     freq_p[p[i]] += 1
+        #     window[s[i]] += 1
+        # 
+        # is_anagram = window == freq_p
+        # if is_anagram: res.append(0)
+        #     
+        # for j in range(n,N):
+        #     # This is_anagram here represents if the previous window value was anagram or not
+        #     
+        #     i = j - n # i represents previous windows starting position
+        #     window[s[i]] -= 1
+        #     window[s[j]] += 1
+        #     
+        #     if window[s[i]]==0: del window[s[i]] # remove character from dict if value is zero
+        #     
+        #     
+        #     # hashtable comparison will take O(1)
+        #     # as max number of comparison is 26 (only lower case alphabets)
+        #     is_anagram = (is_anagram and s[i]==s[j]) or freq_p==window
+        #     
+        #     if is_anagram: res.append(i+1)
+        #         
+        # return res
             
