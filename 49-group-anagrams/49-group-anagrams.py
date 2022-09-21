2e8b33bd2671d8d11fc1bbc9ddcf5665c6d5a109
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-21 20:14:15 +0400

    Time: 249 ms (14.64%), Space: 17.8 MB (67.44%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # using alaphabet array countn as string - keys
+        # using hash of character as keys
         hashmap = defaultdict(list)
         for string in strs:
-            ch_arr = [0]*26
-            for ch in string: ch_arr[ord(ch)-ord('a')] += 1
-            hashmap[''.join(map(chr,ch_arr))].append(string)
+            hashmap[sum(hash(ch) for ch in string)].append(string)
         return hashmap.values()
+    
+        # # using alaphabet array countn as string - keys
+        # hashmap = defaultdict(list)
+        # for string in strs:
+        #     ch_arr = [0]*26
+        #     for ch in string: ch_arr[ord(ch)-ord('a')] += 1
+        #     hashmap[''.join(map(chr,ch_arr))].append(string)
+        # return hashmap.values()
         
         # # using tuple as keys
         # hashmap = defaultdict(list)
@@ -17,8 +23,3 @@ class Solution:
         # return hashmap.values()
         
         
-        # # using hash of character as keys
-        # hashstring = defaultdict(list)
-        # for string in strs:
-        #     hashstring[sum(hash(ch) for ch in string)].append(string)
-        # return hashstring.values()
