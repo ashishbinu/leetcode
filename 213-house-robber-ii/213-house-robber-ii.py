fa1014b94890d871f5644450f0bdb357f90de4cc
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-24 18:13:09 +0400

    Time: 67 ms (15.62%), Space: 13.9 MB (70.97%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -2,19 +2,15 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         # dp[i] represents max money robbed from houses 0..=i such that no two robbed houses are adjacent
         n = len(nums)
-        if n < 1: return 0
         if n == 1: return nums[0]
         
-        a,b = 0,0 # dp[i-2], dp[i-1]
+        a = b = c = d = 0 # a,c = dp[i-2], b,d = dp[i-1]
         for i in range(n-1):
             a,b = b,max(a + nums[i], b)
+            c,d = d,max(c + nums[i+1], d)
             
-        res = b  # dp[n-2] - represents max money robbed from house 0..=n-2
-        a,b = 0,0
-        for i in range(1,n):
-            a,b = b,max(a + nums[i], b)
+        # b = dp[n-2] - represents max money robbed from house 0..=n-2
+        # d = dp[n-1] - represents max money robbed from house 1..=n-1
             
-        # b = dp[n-2] - represents max money robbed from house 1..=n-1
-            
-        return max(res,b)
+        return max(b,d)
         
