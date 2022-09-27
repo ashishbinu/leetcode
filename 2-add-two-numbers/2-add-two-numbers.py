6ac812a7160629377e3063b0d9ef2340292dab17
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-27 13:14:28 +0400

    Time: 115 ms (53.63%), Space: 14.1 MB (9.77%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,8 +5,10 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # # time - O(m + n), space - O(1)
         a,b = l1,l2
-        c = l3 = ListNode() # dummy head
+        start = None
+        end = None
         carry = 0
         
         while a or b:
@@ -16,12 +18,35 @@ class Solution:
             carry = tmp // 10
             tmp %= 10
             
-            c.next = ListNode(val=tmp)
-            c = c.next
+            if a: a.val = tmp
+            if b: b.val = tmp
             
-            a = a.next if a else None
-            b = b.next if b else None
+            if a: a,start,end = a.next,l1,a
+            if b: b,start,end = b.next,l2,b
             
-        if carry: c.next = ListNode(val=carry)
-
-        return l3.next
+        if carry: end.next = ListNode(val=carry)
+        
+        return start
+        
+        
+        # # time - O(m + n), space - O(m + n)
+        # a,b = l1,l2
+        # c = l3 = ListNode() # dummy head
+        # carry = 0
+        # 
+        # while a or b:
+        #     tmp = carry
+        #     if a: tmp += a.val
+        #     if b: tmp += b.val
+        #     carry = tmp // 10
+        #     tmp %= 10
+        #     
+        #     c.next = ListNode(val=tmp)
+        #     c = c.next
+        #     
+        #     a = a.next if a else None
+        #     b = b.next if b else None
+        #     
+        # if carry: c.next = ListNode(val=carry)
+        # 
+        # return l3.next
