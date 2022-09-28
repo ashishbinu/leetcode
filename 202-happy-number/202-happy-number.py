9bd684e70abc8cde7c89553fb3d15a0deb905471
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-28 23:00:06 +0400

    Time: 51 ms (66.91%), Space: 13.8 MB (61.68%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -6,11 +6,21 @@ class Solution:
                 n,d = divmod(n,10)
                 s += d * d
             return s
-                
-        memo = set()
-        while n != 1:
-            memo.add(n)
-            n = sum_of_digit_squares(n)
-            if n in memo: return False
+        
+        # time - O(n), space - O(1)
+        s = f = n
+        while f != 1:
+            s = sum_of_digit_squares(s)  # s = s.next
+            f = sum_of_digit_squares(f)
+            f = sum_of_digit_squares(f)  # f = f.next.next
+            if s == f and s != 1: return False      # cycle detected
         return True
+                
+        # # time - O(n), space - O(n)
+        # memo = set()
+        # while n != 1:
+        #     memo.add(n)
+        #     n = sum_of_digit_squares(n)
+        #     if n in memo: return False
+        # return True
             
