9e61197a435b8c87ace1ad70e8b069150819788e
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-28 23:12:20 +0400

    Time: 70 ms (23.39%), Space: 13.8 MB (61.68%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -9,12 +9,11 @@ class Solution:
         
         # time - O(n), space - O(1)
         
-        s = next(n)
-        f = next(next(n))
-        
-        while s != f:
+        s = f = n
+        while 1:
             s = next(s)        # s = s.next
             f = next(next(f))  # f = f.next.next
+            if s == f: break
             
         return f == 1
                 
