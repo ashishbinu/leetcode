aa06e4fe4725d59ce655a44f3539639d458836cd
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-09-29 22:08:41 +0400

    Time: 345 ms (31.63%), Space: 15.9 MB (47.83%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -6,14 +6,12 @@ class Solution:
         N = len(s)
         n = len(g)
        
-        res = 0
         i = 0
         for j in range(N):
             if i >= n: break
             if s[j] >= g[i]:
-                res += 1
                 i+=1
                 
-        return res
+        return i
         
         
