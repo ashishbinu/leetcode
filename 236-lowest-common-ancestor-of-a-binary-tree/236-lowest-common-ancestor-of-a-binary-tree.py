7a54e3cad8ad1c0ef1057f5ed768957695152976
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-10-04 21:26:01 +0400

    Time: 148 ms (35.29%), Space: 26.3 MB (62.52%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,6 +7,8 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        # returns the lca if p and q exists and if both doesn't exist in tree then return null
         def lca(root,p,q):
             if root is None: return None
             if p == root or q == root: return root
@@ -14,8 +16,10 @@ class Solution:
             right = lca(root.right,p,q)
             left = lca(root.left,p,q)
             
+            # if p and q are in different subtrees (right or left), so lca is root
             if right and left: return root
             
+            # if both p and q are in either right or left
             return right or left
         
         return lca(root,p,q)
