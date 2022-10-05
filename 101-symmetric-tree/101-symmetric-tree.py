b4ae3f7f40d5bbfb6f9c333c54a2bc54ef06bc7e
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-10-05 10:15:24 +0400

    Time: 78 ms (8.47%), Space: 14 MB (21.47%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,32 +6,34 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def dfs(root,res,left):
-            if root is None: return res.append(None)
-            res.append(root.val)
-            if left:
-                dfs(root.left,res,left)
-                dfs(root.right,res,left)
-            else:
-                dfs(root.right,res,left)
-                dfs(root.left,res,left)
-            
-        left = []
-        right = []
-        dfs(root.left,left,True)
-        dfs(root.right,right,False)
-        return left == right
-            
-            
-        # time - O(n), space - O(n)
-        # q = deque([root])
-        # while q:
-        #     pal = []
-        #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         pal.append(node.val if node else None)
-        #         if node:
-        #             q.append(node.left)
-        #             q.append(node.right)
-        #     if pal != pal[::-1]: return False
-        # return True
+        # # time - O(n), space - O(n) - iterative
+        q = deque([root])
+        while q:
+            pal = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                pal.append(node and node.val) # short circuit
+                if node: q += [node.left,node.right]
+                    
+            n = len(pal)        
+            for i in range(n//2):
+                if pal[i] != pal[n-1-i]:
+                    return False
+        return True
+    
+        # # time - O(n), space - O(n + h) - recursive
+        # def dfs(root,res,left):
+        #     if root is None: return res.append(None)
+        #     res.append(root.val)
+        #     if left:
+        #         dfs(root.left,res,left)
+        #         dfs(root.right,res,left)
+        #     else:
+        #         dfs(root.right,res,left)
+        #         dfs(root.left,res,left)
+        #     
+        # left = []
+        # right = []
+        # dfs(root.left,left,True)
+        # dfs(root.right,right,False)
+        # return left == right
