bc03ad4d3b6fe6bf5c1b82621f66d0e203c79e5a
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-10-05 09:59:29 +0400

    Time: 68 ms (28.40%), Space: 14.1 MB (21.47%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,14 +6,32 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        q = deque([root])
-        while q:
-            pal = []
-            for i in range(len(q)):
-                node = q.popleft()
-                pal.append(node.val if node else None)
-                if node:
-                    q.append(node.left)
-                    q.append(node.right)
-            if pal != pal[::-1]: return False
-        return True
+        def dfs(root,res,left):
+            if root is None: return res.append(None)
+            res.append(root.val)
+            if left:
+                dfs(root.left,res,left)
+                dfs(root.right,res,left)
+            else:
+                dfs(root.right,res,left)
+                dfs(root.left,res,left)
+            
+        left = []
+        right = []
+        dfs(root.left,left,True)
+        dfs(root.right,right,False)
+        return left == right
+            
+            
+        # time - O(n), space - O(n)
+        # q = deque([root])
+        # while q:
+        #     pal = []
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         pal.append(node.val if node else None)
+        #         if node:
+        #             q.append(node.left)
+        #             q.append(node.right)
+        #     if pal != pal[::-1]: return False
+        # return True
