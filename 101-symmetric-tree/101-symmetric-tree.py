74ab967d17571dc20ecd363bd18673bdcc0c5fe6
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-10-05 10:24:23 +0400

    Time: 55 ms (59.30%), Space: 14.1 MB (21.47%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,17 +6,26 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # recursive
+        def is_mirror(left,right):
+            if left is None and right is None: return True
+            if left is None or right is None: return False
+            
+            return  left.val == right.val and is_mirror(left.left,right.right) and is_mirror(left.right,right.left)
+        
+        return root is None or is_mirror(root.left,root.right)
+    
         # # time - O(n), space - O(n) - iterative
-        q = deque([root])
-        while q:
-            pal = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                pal.append(node and node.val) # short circuit
-                if node: q += [node.left,node.right]
-                    
-            if pal != pal[::-1]: return False
-        return True
+        # q = deque([root])
+        # while q:
+        #     pal = []
+        #     for _ in range(len(q)):
+        #         node = q.popleft()
+        #         pal.append(node and node.val) # short circuit
+        #         if node: q += [node.left,node.right]
+        #             
+        #     if pal != pal[::-1]: return False
+        # return True
     
         # # time - O(n), space - O(n + h) - recursive
         # def dfs(root,res,left):
