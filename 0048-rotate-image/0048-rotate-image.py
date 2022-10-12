13022cff162805f5366114d6a0609375c534bdd6
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2022-10-12 10:12:42 +0400

    Time: 38 ms (90.86%), Space: 13.9 MB (29.16%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,13 +3,14 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # rotate = matrix[::-1] + transpose
         n = len(matrix)
         # transpose
         for i in range(n):
             for j in range(i+1,n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-        # rotate y axis
+        # rotate y axis from the centre or reverse(which is rotate in x from center)
         for i in range(n):
             for j in range(n//2):
                 matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
