e0ec85dfb42f3bad361deb674cc182d9b5987bae
Ashish Binu <56691622+ashishbinu@users.noreply.github.com>
2023-08-05 21:09:04 +0530

    Time: 1 ms (83.57%), Space: 2.3 MB (7.73%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    
+    pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        
+        let rows = matrix.len();
+        let cols = matrix[0].len();
+        let n = (rows * cols) as i32;
+        let mut l = -1;
+        let mut r = n;
+        
+        let condition = |i: usize| target <= matrix[i / cols][i % cols];
+        
+        while r - l > 1 {
+            let m = (l + r) / 2;
+            
+            if condition(m as usize) {
+                r = m
+            }
+            else {
+                l = m
+            }
+        }
+        
+        !(r == n || matrix[r as usize / cols][r as usize % cols] != target)
+        
+    }
+}
